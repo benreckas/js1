da6348d9156262c765e0b27bd9bc94c068bdc70e
Ben Reckas <benreckas@gmail.com>
2017-10-30 21:04:51 -0700

    Day1 Classwork

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+function logMessage() {
+  console.log('Clicked!');
+};
+
+// Anytime you have code in JS, if it's the end of the execution context,
+// end it with a semi-colon.
+
+// Looking for correct camel casing and semi-colon use.
+
+// Comments are not optional. You MUST DO IT.
+
+/*
+  This is a multi-line
+  comment
+*/
+
+var name = "Ben";
+
+// ES5 String Interpolation
+var message = "Hello there, " + name + "!";
+console.log(message);
+
+// ES6 String Interpolation
+var secondMessage = `Hello there, ${name}!`;
+console.log(secondMessage);
+
+// If you can write less code and it still works the same- do that.
+
+
+// Variable declaration
+
+console.log(number);
+
+var number = 14;
+
+// Variable Definition
