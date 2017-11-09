111277b3f3be62156451e67c1928be8d336f00e8
BEN RECKAS <benreckas@BENs-MacBook-Pro.local>
2017-11-08 21:02:29 -0800

    Day 4 in class work and notes.

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+// This is an array
+// var ryan = "is awesome";
+// var myArray = [23, "Hello World!", ryan];
+
+
+// var benArray = ["John Wick 2", 6, true, 2007]
+// var tvShow = "Stranger Things";
+// benArray.push(tvShow);
+// myArray.pop();
+//
+//
+// console.log(benArray, benArray.length, Array.isArray(benArray));
+
+
+// splice is super useful
+// array.splice(start, deleteCount, item1, item2, ...)
+
+var favMovies = [];
+var movie1 = ["Inglorious Bastards", "Aldo the Apache"];
+var movie2 = ["Hell or High Water", "Toby Howard"];
+var movie3 = ["Shawshank Redemption", "Andy Duframe"];
+var movie4 = ["The Departed", "Billy"];
+var movie5 = ["Road to Perdition", "Michael Sullivan"];
+favMovies.splice(0, 0, movie1, movie2, movie3, movie4, movie5);
+console.log(favMovies);
+
+var movieTitles = []
+movieTitles.push = [favMovies[0][0], favMovies[1][0], favMovies[2][0], favMovies[3][0], favMovies[4][0]];
+console.log(movieTitles);
+
+var favAndLeastFav = [];
+favAndLeastFav.push(favMovies[0], favMovies[4]);
+console.log(favAndLeastFav);
+
+
+// loops can be used on arrays and objects
+
+// while (condition) {
+//   something happens
+// }
+
+
+var arrClasses = ["Websites for Beginners", "Mobile Friendly Websites", "JavaScript", "Angular"]
+
+let i = 0
+while (i < arrClasses.length) {
+  console.log(i);
+  console.log(arrClasses[i]);
+  i++;
+}
