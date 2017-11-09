f555bdc9cadcfd694a7f35689ee8c1881cc573dd
BEN RECKAS <benreckas@BENs-MacBook-Pro.local>
2017-11-08 21:03:24 -0800

    Day 4 in class.

--- js/scripts.js
+++ js/scripts.js
@@ -15,22 +15,22 @@
 // splice is super useful
 // array.splice(start, deleteCount, item1, item2, ...)
 
-var favMovies = [];
-var movie1 = ["Inglorious Bastards", "Aldo the Apache"];
-var movie2 = ["Hell or High Water", "Toby Howard"];
-var movie3 = ["Shawshank Redemption", "Andy Duframe"];
-var movie4 = ["The Departed", "Billy"];
-var movie5 = ["Road to Perdition", "Michael Sullivan"];
-favMovies.splice(0, 0, movie1, movie2, movie3, movie4, movie5);
-console.log(favMovies);
-
-var movieTitles = []
-movieTitles.push = [favMovies[0][0], favMovies[1][0], favMovies[2][0], favMovies[3][0], favMovies[4][0]];
-console.log(movieTitles);
-
-var favAndLeastFav = [];
-favAndLeastFav.push(favMovies[0], favMovies[4]);
-console.log(favAndLeastFav);
+// var favMovies = [];
+// var movie1 = ["Inglorious Bastards", "Aldo the Apache"];
+// var movie2 = ["Hell or High Water", "Toby Howard"];
+// var movie3 = ["Shawshank Redemption", "Andy Duframe"];
+// var movie4 = ["The Departed", "Billy"];
+// var movie5 = ["Road to Perdition", "Michael Sullivan"];
+// favMovies.splice(0, 0, movie1, movie2, movie3, movie4, movie5);
+// console.log(favMovies);
+//
+// var movieTitles = []
+// movieTitles.push = [favMovies[0][0], favMovies[1][0], favMovies[2][0], favMovies[3][0], favMovies[4][0]];
+// console.log(movieTitles);
+//
+// var favAndLeastFav = [];
+// favAndLeastFav.push(favMovies[0], favMovies[4]);
+// console.log(favAndLeastFav);
 
 
 // loops can be used on arrays and objects
@@ -40,11 +40,18 @@ console.log(favAndLeastFav);
 // }
 
 
-var arrClasses = ["Websites for Beginners", "Mobile Friendly Websites", "JavaScript", "Angular"]
+// var arrClasses = ["Websites for Beginners", "Mobile Friendly Websites", "JavaScript", "Angular"];
 
-let i = 0
-while (i < arrClasses.length) {
-  console.log(i);
-  console.log(arrClasses[i]);
-  i++;
-}
+// let i = 0
+// while (i < favMovies.length) {
+//   favMovies
+//   if(favMovies[i][0] == "Inglorious Bastards" || favMovies[i][0] == "Road to Perdition") {
+//   console.log(i);
+  // console.log(arrClasses[i]);
+//   }
+//   i++;
+// }
+
+// for (let i = 0; i <= favMovies.length ; i++) {
+//   console.log(arrClasses[i]);
+// }
