c17090a5de83a803392be3da84989dc8c4bbc036
benreckas <benreckas@gmail.com>
2017-11-27 20:39:28 -0800

    day7 in class.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
-var div = document.querySelector('.text');
+// var div = document.querySelector('.text');
 //
 // div.innerHTML = '<h1>Hey.</h1>';
 
-var myNewPTag = document.createElement('p');
+// var myNewPTag = document.createElement('p');
 // var myDiv = document.createElement('div');
 // myDiv.textContent = 'Star Wars is so cool...: ()';
-myNewPTag.textContent = "Stuff";
+// myNewPTag.textContent = "Stuff";
 // div.appendChild(myNewPTag);
 // div.append(myDiv, myNewPTag);
 
-var ul = document.querySelector('ul');
-var lis = document.querySelectorAll('li');
-ul.insertBefore(myNewPTag, lis[2]);
-
-
-myNewPTag.className = 'text-center';
-
-document.addEventListener('click', (e) => {
+// var ul = document.querySelector('ul');
+// var lis = document.querySelectorAll('li');
+// ul.insertBefore(myNewPTag, lis[2]);
+//
+//
+// myNewPTag.className = 'text-center';
+//
+// document.addEventListener('click', (e) => {
   // myNewPTag.classList.toggle('bg-success');
-});
+// });
 
 
 // var myImg = document.createElement('img');
