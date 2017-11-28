0586ea6f43b0a8bbf5d82a9249e2b560f132fd9a
benreckas <benreckas@gmail.com>
2017-11-27 19:50:48 -0800

    In class work.

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+var div = document.querySelector('.text');
+//
+// div.innerHTML = '<h1>Hey.</h1>';
+
+var myNewPTag = document.createElement('p');
+// var myDiv = document.createElement('div');
+// myDiv.textContent = 'Star Wars is so cool...: ()';
+myNewPTag.textContent = "Stuff";
+// div.appendChild(myNewPTag);
+// div.append(myDiv, myNewPTag);
+
+var ul = document.querySelector('ul');
+var lis = document.querySelectorAll('li');
+ul.insertBefore(myNewPTag, lis[2]);
+
+
+myNewPTag.className = 'text-center';
+
+document.addEventListener('click', (e) => {
+  // myNewPTag.classList.toggle('bg-success');
+});
+
+
+// var myImg = document.createElement('img');
+// myImg.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/en/7/7f/Star_Wars_The_Last_Jedi.jpg');
+// div.appendChild('myImg');
